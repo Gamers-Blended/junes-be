44508cc3fd478d08f297c4c29c894ee1f65b8552
AB <wong_zhihang@u.nus.edu>
2025-04-02 02:10:07 +0800

    Initial commit

---
+++ src/main/java/com/gamersblended/junes/JunesApplication.java
@@ -0,0 +1,13 @@
+package com.gamersblended.junes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunesApplication.class, args);
+	}
+
+}
