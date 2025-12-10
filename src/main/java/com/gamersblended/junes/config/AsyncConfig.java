d030f9773e012069f1a9a1314f1a9b30c7e78f2a
Gamers-Blended <zhihangwong@proton.me>
2025-12-11 07:48:22 +0800

    [MFLP-68] refactor: remove hard-coded async config values

--- src/main/java/com/gamersblended/junes/config/AsyncConfig.java
+++ src/main/java/com/gamersblended/junes/config/AsyncConfig.java
@@ -1,24 +1,17 @@
 package com.gamersblended.junes.config;
 
+import org.springframework.boot.task.ThreadPoolTaskExecutorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
 
 @EnableAsync
 @Configuration
 public class AsyncConfig {
 
     @Bean(name = "taskExecutor")
-    public Executor taskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(5);
-        executor.setQueueCapacity(150);
-        executor.setThreadNamePrefix("email-async-");
-        executor.initialize();
-        return executor;
+    public ThreadPoolTaskExecutor taskExecutor(ThreadPoolTaskExecutorBuilder builder) {
+        return builder.build();
     }
 }
