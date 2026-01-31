f3b3c31ee5f64bb9a964b7634252849490a83551
Gamers-Blended <zhihangwong@proton.me>
2026-02-01 03:10:56 +0800

    [MFLP-158] feat: add manual trigger for housekeeping unverified emails job (admin-only)

--- src/main/java/com/gamersblended/junes/controller/HouseKeepController.java
+++ src/main/java/com/gamersblended/junes/controller/HouseKeepController.java
@@ -1,8 +1,8 @@
 package com.gamersblended.junes.controller;
 
-import com.gamersblended.junes.dto.TransactionHistoryDTO;
 import com.gamersblended.junes.dto.reponse.ResponseMessage;
 import com.gamersblended.junes.exception.DatabaseDeletionException;
+import com.gamersblended.junes.service.EmailVerificationTokenService;
 import com.gamersblended.junes.service.PasswordResetService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -21,16 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class HouseKeepController {
 
     private final PasswordResetService passwordResetService;
+    private final EmailVerificationTokenService emailVerificationTokenService;
 
-    public HouseKeepController(PasswordResetService passwordResetService) {
+    public HouseKeepController(PasswordResetService passwordResetService, EmailVerificationTokenService emailVerificationTokenService) {
         this.passwordResetService = passwordResetService;
+        this.emailVerificationTokenService = emailVerificationTokenService;
     }
 
     @Operation(summary = "Manually trigger housekeeping of expired tokens")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Blacklisted tokens cleared",
                     content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = TransactionHistoryDTO.class))}),
+                            schema = @Schema(implementation = ResponseMessage.class))}),
             @ApiResponse(responseCode = "500", description = "Error in deleting expired tokens",
                     content = {@Content(mediaType = "application/json",
                             schema = @Schema(implementation = DatabaseDeletionException.class))})
@@ -41,5 +43,22 @@ public class HouseKeepController {
         log.info("Starting house keeping for blacklisted tokens...");
         passwordResetService.cleanupExpiredTokens();
         return ResponseEntity.ok(new ResponseMessage("Blacklisted tokens cleared"));
+    }
+
+    @Operation(summary = "Manually trigger housekeeping of unverified emails")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Unverified emails cleared",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseMessage.class))}),
+            @ApiResponse(responseCode = "500", description = "Error in deleting unverified emails",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DatabaseDeletionException.class))})
+    })
+    @PostMapping("/unverified-emails")
+    public ResponseEntity<ResponseMessage> houseKeepUnverifiedEmails() {
+
+        log.info("Starting house keeping for unverified emails...");
+        emailVerificationTokenService.cleanupUnverifiedEmails();
+        return ResponseEntity.ok(new ResponseMessage("Unverified emails cleared"));
     }
 }
