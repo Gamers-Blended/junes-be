be308a117626dcb3fafdbbae9f673534c6b35808
Gamers-Blended <zhihangwong@proton.me>
2026-02-07 17:48:08 +0800

    [MFLP-163] feat: remove @Size validation check for Create User, Login and Password Request requests

--- src/main/java/com/gamersblended/junes/dto/request/LoginRequest.java
+++ src/main/java/com/gamersblended/junes/dto/request/LoginRequest.java
@@ -1,9 +1,7 @@
 package com.gamersblended.junes.dto.request;
 
-import com.gamersblended.junes.constant.ValidationConstants;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,8 +14,5 @@ public class LoginRequest {
     private String email;
 
     @NotBlank(message = "Password is required")
-    @Size(min = ValidationConstants.PASSWORD_MIN_LENGTH, max = ValidationConstants.PASSWORD_MAX_LENGTH,
-            message = "Password must be between " + ValidationConstants.PASSWORD_MIN_LENGTH + " and "
-                    + ValidationConstants.PASSWORD_MAX_LENGTH + "characters")
     private String password;
 }
