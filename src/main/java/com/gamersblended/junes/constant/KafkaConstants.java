24946a60382ce16bb72349ffdea93803c0ca3335
Gamers-Blended <zhihangwong@proton.me>
2026-07-19 21:21:21 +0800

    [MFLP-186] feat: add ORDER_CREATED event type

--- src/main/java/com/gamersblended/junes/constant/KafkaConstants.java
+++ src/main/java/com/gamersblended/junes/constant/KafkaConstants.java
@@ -5,5 +5,6 @@ public class KafkaConstants {
     private KafkaConstants () {};
 
     public static final String INVENTORY_CHANGED = "INVENTORY_CHANGED";
+    public static final String ORDER_CREATED = "ORDER_CREATED";
     public static final String ORDER_PLACED = "ORDER_PLACED";
 }
