116ad908f147cff79dada98c80c7918060a1927f
Gamers-Blended <zhihangwong@proton.me>
2026-08-18 02:07:13 +0800

    [MFLP-203] feat: add detach Payment Method Event

---
+++ src/main/java/com/gamersblended/junes/dto/event/StripePaymentMethodDetachEvent.java
@@ -0,0 +1,21 @@
+package com.gamersblended.junes.dto.event;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+import static com.gamersblended.junes.constant.KafkaConstants.PAYMENT_METHOD_DETACHED;
+
+@Data
+public class StripePaymentMethodDetachEvent extends BaseEvent {
+
+    private String eventID;
+    private Integer schemaVersion; // For updates on payload shape
+    private UUID userID;
+    private String stripePaymentMethodID;
+    private UUID paymentMethodID;
+
+    public StripePaymentMethodDetachEvent() {
+        this.setEventType(PAYMENT_METHOD_DETACHED);
+    }
+}
