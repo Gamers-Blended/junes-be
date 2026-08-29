0f9f90b48a008ee05e9a28767d7c682ea581e32d
Gamers-Blended <zhihangwong@proton.me>
2026-08-29 16:14:40 +0800

    [MFLP-233] feat: add infra for logging correlation IDs

---
+++ src/main/java/com/gamersblended/junes/constant/LoggingConstants.java
@@ -0,0 +1,13 @@
+package com.gamersblended.junes.constant;
+
+public class LoggingConstants {
+
+    private LoggingConstants() {
+    }
+
+    // HTTP correlation ID header
+    public static final String CORRELATION_ID_HEADER = "X-Correlation-Id";
+
+    // MDC key
+    public static final String MDC_CORRELATION_ID_KEY = "correlationId";
+}
