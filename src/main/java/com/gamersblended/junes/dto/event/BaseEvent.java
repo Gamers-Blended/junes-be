1485ee862bdd172c7e94ae20aac3a9fcfd384d89
Gamers-Blended <zhihangwong@proton.me>
2026-08-29 16:15:48 +0800

    [MFLP-233] feat: add correlationId to BaseEvent

--- src/main/java/com/gamersblended/junes/dto/event/BaseEvent.java
+++ src/main/java/com/gamersblended/junes/dto/event/BaseEvent.java
@@ -1,10 +1,13 @@
 package com.gamersblended.junes.dto.event;
 
 import lombok.Data;
+import org.slf4j.MDC;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.UUID;
+
+import static com.gamersblended.junes.constant.LoggingConstants.MDC_CORRELATION_ID_KEY;
 
 @Data
 public class BaseEvent {
@@ -13,4 +16,5 @@ public class BaseEvent {
     private LocalDateTime timestamp = LocalDateTime.now(ZoneId.of("Asia/Singapore"));
     private String eventType;
     private String idempotencyKey;
+    private String correlationId = MDC.get(MDC_CORRELATION_ID_KEY);
 }
