aebd5d462f3a97ec69d932feb058b437c3072211
Gamers-Blended <zhihangwong@proton.me>
2026-09-11 20:16:49 +0800

    refactor: remove unused exceptions and ResponseStatus annotations

--- src/main/java/com/gamersblended/junes/exception/WebClientRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.gamersblended.junes.exception;
-
-public class WebClientRequestException extends RuntimeException {
-    public WebClientRequestException(String message) {
-        super(message);
-    }
-}
